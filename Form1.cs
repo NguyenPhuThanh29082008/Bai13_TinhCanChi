2a5fce010995374ab28e8b97f52abb752ab40586
NguyenPhuThanh29082008 <nguyenphuthanh298@gmail.com>
2025-02-24 13:28:33 +0700

    Update

--- Form1.cs
+++ Form1.cs
@@ -24,7 +24,17 @@ namespace Bai13_TinhCanChi
 
             string giatritrave = canArr[Convert.ToInt32(txtNam.Text) % 10] + chiArr[Convert.ToInt32(txtNam.Text) % 12];
             txtKQ.Text = giatritrave;
+
+
+
+            for (int nam = 2000; nam <= 2050; nam++)
+            {
+                lstNam.Items.Add(nam + " - " + canArr[nam % 10] + chiArr[nam % 12]);
+            }
         }
+   
+
+        
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
@@ -38,15 +48,12 @@ namespace Bai13_TinhCanChi
 
         private void lstNam_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string[] canArr = { "Canh ", "Tân ", "Nhâm ", "Qúy ", "Giáp ", "Ất ", "Bính ", "Đinh ", "Mậu ", "Kỵ " };
-            string[] chiArr = { "Thân", "Dậu", "Tuất", "Hợi", "Tý", "Sửu", "Dần", "Mảo", "Thìn", "Tý", "Ngọ", "Mùi" };
 
-            string giatritrave = canArr[Convert.ToInt32(txtNam.Text) % 10] + chiArr[Convert.ToInt32(txtNam.Text) % 12];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
             
-            for (int nam = 2000; nam <= 2050; nam++)
-            {
-                lstNam.Items.Add (nam);
-            }
         }
     }
 }
